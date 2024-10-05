579a895ca9c7e3ecec94b635728444c4545310c8
far1h <farihmuhammad45@gmail.com>
2024-10-05 17:23:48 +0700

    Add a new REST endpoint to app

--- src/main/java/com/farihmhmd/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/farihmhmd/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
     // expose "/" that returns "Hello World"
 
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
     }
 }
